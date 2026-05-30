c28d42bc39796474dad8a24e1f1338879316dab6
pikrih <fikriichsan89@gmail.com>
2026-05-30 17:56:13 +0700

    first commit

--- index.js
+++ index.js
@@ -29,7 +29,14 @@ const fileFilter = (req, file, cb) => {
 };
 const upload = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } });
 
-const db = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'db_univora' });
+const db = mysql.createConnection({
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 db.connect((err) => {
     if (err) { console.error('Database gagal terhubung:', err); }
     else {
