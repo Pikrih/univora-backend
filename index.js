eef2e221d13341b7730cc2833407f279a502c669
pikrih <fikriichsan89@gmail.com>
2026-05-30 17:59:31 +0700

    fix db connection use env variables

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const upload = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024
 
 const db = mysql.createConnection({
   host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
+  port:     parseInt(process.env.DB_PORT) || 3306,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
